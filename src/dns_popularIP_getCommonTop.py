71da0dabd09c65433c161299b5ed20d326e54a7e
weisser <94apieceofcake@gmail.com>
2018-06-23 20:04:40 -0600

    add new methods to generate common popular IPs

---
+++ src/dns_popularIP_getCommonTop.py
@@ -0,0 +1,48 @@
+from src.dns_popularIP_tidy import tidyPopIP
+from src.dns_popularIP_getTop import popIPTop
+
+def get_common_list(ip_list1, ip_list2):
+    """
+    return a list of common elements of two different lists.
+    :param ip_list1:
+    :param ip_list2:
+    :return:
+    """
+    return [element for element in ip_list1 if element in ip_list2]
+    # return list(set(ip_list1) - (set(ip_list1) - set(ip_list2)))
+
+
+def get_common_IPList(cookie, ts1,ts2, percent):
+        filename1 = "../data/result_popIP/%s/%s_tidy.log" % (ts1, cookie)
+        ipTop1 = popIPTop(ts1, cookie, percent, tidyPopIP(filename1))
+        ip_list1 = ipTop1.get_pop_list()
+
+        filename2 = "../data/result_popIP/%s/%s_tidy.log" % (ts2, cookie)
+        ipTop2 = popIPTop(ts2, cookie, percent, tidyPopIP(filename2))
+        ip_list2 = ipTop2.get_pop_list()
+
+        common_list = get_common_list(ip_list1, ip_list2)
+        return common_list
+
+if __name__ == '__main__':
+    timestamp_list = ["2018-03-01_12", "2018-03-08_10", "2018-03-08_12",
+                      "2018-03-08_16", "2018-03-07_12", "2018-03-16_12"]
+
+    cookie_list = ["in_src", "in_dst", "out_src", "out_dst"]
+    cookie_index = 3
+
+    percent = 90
+
+    common_list = get_common_IPList(cookie_list[cookie_index], timestamp_list[0], timestamp_list[1], percent)
+
+    for timestamp in timestamp_list[2:]:
+        filename = "../data/result_popIP/%s/%s_tidy.log" % (timestamp, cookie_list[cookie_index])
+        ipTop = popIPTop(timestamp, cookie_list[cookie_index], percent, tidyPopIP(filename))
+        ip_list = ipTop.get_pop_list()
+
+        common_list = get_common_list(common_list, ip_list)
+
+    print(common_list)
+
+
+
--- src/dns_popularIP_getTop.py
+++ src/dns_popularIP_getTop.py
@@ -1,10 +1,12 @@
-from src.dns_popular_IP_tidy import tidyPopIP
+from src.dns_popularIP_tidy import tidyPopIP
 
 
 class popIPTop(object):
-    def __init__(self, percent, tidyIPDict):
+    def __init__(self, timestamp, cookie, percent, tidyIPDict):
         if percent < 0 or percent > 100:
             raise ValueError("input percent not between 0 and 100")
+        self.timestamp = timestamp
+        self.cookie = cookie
         self.percent = percent
         self.tidyIPDict = tidyIPDict
 
@@ -18,15 +20,32 @@ class popIPTop(object):
             self.popIPList.append(self.tidyIPDict.popIP_dict[index][1])
             count_remain -= self.tidyIPDict.popIP_dict[index][0]
             index += 1
-        print(self.popIPList)
-        print(len(self.popIPList))
+        # print(self.popIPList)
+        # print(len(self.popIPList))
+        return self.popIPList
+
+    def store_pop_list(self):
+        output_file = "../result/result_topIP/%s/%s_top_%d.log" % (self.timestamp, self.cookie, self.percent)
+        with open(output_file, 'a') as f:
+            for ip in self.popIPList:
+                f.write(ip + "\n")
 
 
 if __name__ == '__main__':
-    timestamp = "2018-03-07_12"
-    percent = 80
-    cookie = "out_src"
+    # timestamp = "2018-03-01_12"
 
-    filename = "../data/result_popIP/%s/%s_tidy.log" % (timestamp, cookie)
-    ipTop = popIPTop(percent, tidyPopIP(filename))
-    ipTop.get_pop_list()
+    # cookie = "out_src"
+
+    timestamp_list = ["2018-03-01_12", "2018-03-08_10", "2018-03-08_12",
+                      "2018-03-08_16", "2018-03-07_12", "2018-03-16_12"]
+
+    cookie_list = ["in_src", "in_dst", "out_src", "out_dst"]
+
+    percent = 90
+
+    for timestamp in timestamp_list:
+        for cookie in cookie_list:
+            filename = "../data/result_popIP/%s/%s_tidy.log" % (timestamp, cookie)
+            ipTop = popIPTop(timestamp, cookie, percent, tidyPopIP(filename))
+            ipTop.get_pop_list()
+            ipTop.store_pop_list()
